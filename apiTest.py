a7dba011d10211dcea2405fa5d88ed26d4095f89
itsme12453 <heetvirola110508@gmail.com>
2023-01-28 11:03:29 +0000

    Laptop

--- apiTest.py
+++ apiTest.py
@@ -1,6 +1,6 @@
 import requests
 
 # DSH$32Rfdhu@'34
-request = requests.post("https://fitness-app-hxjw.onrender.com/register", headers={ "authentication-key": "DSH$32Rfdhu@'34", "email": "123@gmail.com", "password": "123£$dsaa", "fullname": "123" })
+request = requests.post("https://fitness-api.itsme12453.repl.co/register", headers={ "authentication-key": "DSH$32Rfdhu@'34", "email": "1234@gmail.com", "password": "123£$dsaa", "fullname": "123" })
 
 print(request.text)
